cb458d730bfefbf3e096879efedeb45921ac2cee
jptolozac <jeanpaultoloza@gmail.com>
2023-07-25 00:27:18 -0500

    prueba

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function login(Request $request){
-        //dd('holii');
+        dd('holii');
         $this->validateLogin($request);
         $credenciales = $request->only('email', 'password');
 
